4bde7d06a8fa57bf17c40db19e95307ea2c3208a
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2012-06-10 10:32:51 +0900

    no comment

--- vending.rb
+++ vending.rb
@@ -5,6 +5,10 @@ class Vending
     @amount = 0
   end
   attr_reader :amount
+
+  def drop_in(money)
+    @amount += money
+  end
 end
 
 class TestVending < Test::Unit::TestCase
@@ -14,5 +18,13 @@ class TestVending < Test::Unit::TestCase
 
   def test_core
     assert_equal(0, @v.amount)
+
+    @v.drop_in(10)
+    assert_equal(10, @v.amount)
+
+    @v.drop_in(10)
+    @v.drop_in(100)
+    assert_equal(120, @v.amount)
+    
   end
 end
