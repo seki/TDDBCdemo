e4472b3fa1710025bf22aa9c92100464404dd1ee
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2012-06-10 10:36:43 +0900

    no comment

--- vending.rb
+++ vending.rb
@@ -1,12 +1,14 @@
 require 'test/unit'
 
 class Vending
-  Item = Struct.new(:price)
+  Item = Struct.new(:price, :stock, :name)
 
   def initialize
     @amount = 0
     @item = Item.new
     @item.price = 120
+    @item.stock = 5
+    @item.name = 'Cola'
   end
   attr_reader :amount, :item
 
@@ -23,6 +25,9 @@ class TestVending < Test::Unit::TestCase
   def test_buy
     assert_equal(0, @v.amount)
     assert_equal(120, @v.item.price)
+    assert_equal(5, @v.item.stock)
+    assert_equal('Cola', @v.item.name)
+
 
     @v.drop_in(10)
     @v.drop_in(10)
